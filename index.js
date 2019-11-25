aab6f4e4eff88e84e43add8e0b1aa8f408d27e6a
Pasha <admin@MacBook-Pro-admin.local>
2019-11-26 00:05:34 +0200

    add project

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+/*const http = require('http')
 const hostname = '127.0.0.1'
 const port = 3000
 const server = http.createServer((req, res) => {
@@ -8,4 +8,27 @@ const server = http.createServer((req, res) => {
 })
 server.listen(port, hostname, () => {
  console.log(`Server running at http://${hostname}:${port}/`)
+})*/
+
+const PORT = process.env.PORT || 3000;
+const http = require('http')
+const fs = require("fs");
+const hostname = '127.0.0.1'
+const port = 3000
+const server = http.createServer((req, response) => {
+ response.statusCode = 200
+ response.setHeader("Content-Type", "text/html")
+ fs.readFile("/Users/admin/Desktop/laba5-Node-JS/Laba 7/index.html", null, (error, data) => {
+  if (error) {
+    response.writeHead(404);
+    response.write("Whoops! File not found!");
+  } else {
+    response.write(data);
+  }
+  response.end();
+  });
+ //res.end('Hi there!\n')
+})
+server.listen(PORT, () => {
+ console.log(`Server running`)
 })
